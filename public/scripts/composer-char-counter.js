675dc9cdfdf89f970cc07daa4a9c4f350359ff09
tjbeirao <tjbeirao@gmail.com>
2018-01-31 04:30:54 +0000

    Logo OK, compose tweet layout OK, new tweet layout OK. Implementing HOVER action for new tweet layout

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+$('input').on('click', () => {    
+})
+
+$("textarea").on("keyup", function() {
+    $("main span").text((140 - $(this).val().length));
+    if ((140 - $(this).val().length) < 0) {
+        $("main span").addClass('invalid');
+    } else {
+        $("main span").removeClass("invalid");
+    }
+});
+
+});
