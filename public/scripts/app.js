d7213eebd3cfd0707c905bbdfc97e1597b96857e
tjbeirao <tjbeirao@gmail.com>
2018-01-31 00:45:53 +0000

    Implemented hover on tweeters, implemented icons to share/like/flag. Next step is set the functions to create tweeters

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,20 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+// $(document).ready(function() {
+
+// $(".show-tweets header").mouseover(function() {
+//     $(this).css("background-color","darkgrey");
+// }).mouseout(function() {
+//     $(this).css("background-color","#eeeeee");
+// });
+
+
+
+    
+
+
+
+
+// });
