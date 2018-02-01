211f91bee6ca3f9a2596ae2f1624aef4eb2a3868
tjbeirao <tjbeirao@gmail.com>
2018-02-01 20:44:43 +0000

    MongoDB implemented, async server is working. Need to fix time on tweets

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,19 +9,33 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      // db.tweets.insertOne({newTweet});
+      db.collection("tweets").insertOne(newTweet);
+      callback(null, true);
     },
 
     // Get all tweets in `db`, sorted by newest first
+    
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, tweets);
       });
     }
-
+    
   };
 }
+
+
+// getTweets((err, tweets) => {
+//   if (err) throw err;
+
+//   console.log("Logging each tweet:");
+//   for (let tweet of tweets) {
+//     console.log(tweet);
+//   }
+
+//   db.close();
+// }); */
